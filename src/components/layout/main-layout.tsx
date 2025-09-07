b607077450f077abaddf3ca6eb9ecd133eaecf7d
عمر المطيري <nntn127@gmail.com>
2025-09-07 08:45:15 +0000

    404 This page could not be found.

--- src/components/layout/main-layout.tsx
+++ src/components/layout/main-layout.tsx
@@ -1,7 +1,7 @@
 import { SidebarProvider } from "@/components/ui/sidebar";
 import { SidebarNav } from "@/components/layout/sidebar-nav";
 
-export default function MainLayout({
+export function MainLayout({
   children,
 }: {
   children: React.ReactNode;
