9afc093e251164fbc630600173c8e6f104d6ea03
عمر المطيري <nntn127@gmail.com>
2025-09-07 03:18:05 +0000

    نعم

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+// app/page.tsx
+import PatientsDashboardPage from "./(dashboard)/patients/page";
+
+export default function Home() {
+  return (
+    <main>
+      <PatientsDashboardPage />
+    </main>
+  );
+}
